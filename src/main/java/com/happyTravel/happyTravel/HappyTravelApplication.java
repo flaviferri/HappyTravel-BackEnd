2dac8d915ff16b791f5c7c2cedbeed23ef7f19c5
“Flavia <flaviferri90@gmail.com>
2024-08-22 09:14:25 +0200

    main Updated

---
+++ src/main/java/com/happyTravel/happyTravel/HappyTravelApplication.java
@@ -0,0 +1,13 @@
+package com.happyTravel.happyTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happyTravel/happyTravel/HappyTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyTravel.happyTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
