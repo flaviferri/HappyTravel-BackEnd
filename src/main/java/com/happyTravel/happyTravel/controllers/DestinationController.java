317b02d6df13ad52bed2bc4c6ca480c1e3d0b47d
sarii4 <sarita248@outlook.es>
2024-08-29 11:11:08 +0200

    delete no used imports

--- src/main/java/com/happyTravel/happyTravel/controllers/DestinationController.java
+++ src/main/java/com/happyTravel/happyTravel/controllers/DestinationController.java
@@ -1,20 +1,21 @@
 package com.happyTravel.happyTravel.controllers;
 
-import com.happyTravel.happyTravel.models.Destination;
-import com.happyTravel.happyTravel.services.DestinationService;
+import java.util.List;
+import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.happyTravel.happyTravel.models.Destination;
+import com.happyTravel.happyTravel.services.DestinationService;
 
 
 
